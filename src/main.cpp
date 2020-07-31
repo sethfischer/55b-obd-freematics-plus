5fd1b5c394038e1b7153183d2cb62ea2a353fb69
Seth Fischer <seth@fischer.nz>
2020-07-31 17:25:20 +1200

    Generate compiler error if WiFi env vars are not set

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <Arduino.h>
 
+#ifndef X55B_WIFI_SSID
+#error 'Environment variable X55B_WIFI_SSID is not defined'
+#endif
+
+#ifndef X55B_WIFI_PASS
+#error 'Environment variable X55B_WIFI_PASS is not defined'
+#endif
+
 void setup() {}
 
 void loop() {}
