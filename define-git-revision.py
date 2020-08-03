bc7a41130a9a5060c7bc4fed2411e0bbb0fddefe
Seth Fischer <seth@fischer.nz>
2020-08-03 22:28:46 +1200

    Define X55B_SRC_REVISION and X55B_SRC_STATE

---
+++ define-git-revision.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Git revision and working tree state"""
+
+import subprocess
+
+
+REVISION = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+
+if subprocess.check_output(['git', 'diff', '--stat']).strip() == '':
+    STATE = 'clean'
+else:
+    STATE = 'dirty'
+
+
+print('-D X55B_SRC_REVISION=\'"{}"\''.format(REVISION.decode('UTF-8')))
+print('-D X55B_SRC_STATE=\'"{}"\''.format(STATE))
