d54f1fa8f6e67da96e220826a19753b1d7bc18da
Seth Fischer <seth@fischer.nz>
2020-08-04 17:21:09 +1200

    Format with Black

--- define-git-revision.py
+++ define-git-revision.py
@@ -4,13 +4,13 @@
 import subprocess
 
 
-REVISION = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+REVISION = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
 
-if subprocess.check_output(['git', 'diff', '--stat']).strip() == '':
-    STATE = 'clean'
+if subprocess.check_output(["git", "diff", "--stat"]).strip() == "":
+    STATE = "clean"
 else:
-    STATE = 'dirty'
+    STATE = "dirty"
 
 
-print('-D X55B_SRC_REVISION=\'"{}"\''.format(REVISION.decode('UTF-8')))
-print('-D X55B_SRC_STATE=\'"{}"\''.format(STATE))
+print("-D X55B_SRC_REVISION='\"{}\"'".format(REVISION.decode("UTF-8")))
+print("-D X55B_SRC_STATE='\"{}\"'".format(STATE))
