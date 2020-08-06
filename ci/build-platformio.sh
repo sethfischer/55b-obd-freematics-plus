38ec1a3507ada7b951cc10b5f7dffed0e7160c63
Seth Fischer <seth@fischer.nz>
2020-08-06 17:45:23 +1200

    Python version 3.6

--- ci/build-platformio.sh
+++ ci/build-platformio.sh
@@ -6,7 +6,7 @@ if [ "${GITHUB_WORKSPACE}x" != "x" ]; then
     cd "$GITHUB_WORKSPACE" || exit
     export PATH=$PATH:~/.platformio/penv/bin
     curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py -o get-platformio.py
-    python3 get-platformio.py
+    python get-platformio.py
 fi
 
 pio platform install "espressif32"
