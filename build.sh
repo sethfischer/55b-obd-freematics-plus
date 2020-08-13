d2ab9d1f0f20dd8bd1454d72f9c7f607c4a53d21
Seth Fischer <seth@fischer.nz>
2020-08-13 19:42:14 +1200

    Install PlatformIO as part of GitHub Workflow

--- build.sh
+++ build.sh
@@ -2,13 +2,4 @@
 
 set -o errexit -o noclobber
 
-if [ "${GITHUB_WORKSPACE}x" != "x" ]; then
-    cd "$GITHUB_WORKSPACE" || exit
-    export PATH=$PATH:~/.platformio/penv/bin
-    curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py -o get-platformio.py
-    python get-platformio.py
-fi
-
-platformio platform install "espressif32"
-
 platformio run --environment=freematics_one_plus --environment=freematics_one_plus_debug
