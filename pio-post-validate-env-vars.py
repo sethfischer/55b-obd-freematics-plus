40a39cd4fae6d2a2f5710046affa2335b35e4ac6
Seth Fischer <seth@fischer.nz>
2020-08-14 17:07:00 +1200

    Define CI env vars in GitHub Workflow

--- pio-post-validate-env-vars.py
+++ pio-post-validate-env-vars.py
@@ -14,6 +14,7 @@ name_prefix = "X55B"
 defines = {}
 error_messages = []
 
+
 for define in projenv["CPPDEFINES"]:
     if type(define) is tuple:
         if define[0].startswith(name_prefix):
@@ -22,30 +23,14 @@ for define in projenv["CPPDEFINES"]:
         if define.startswith(name_prefix):
             defines[define] = None
 
-if env.get("ENV").get("CI") is None:
-    ci_environment = False
-else:
-    ci_environment = True
 
-if ci_environment:
-    print("CI environment detected:")
+print("Checking required macros are set:")
 
-    projenv.ProcessUnFlags("-DX55B_WIFI_SSID=")
-    projenv.Append(CPPDEFINES=[("X55B_WIFI_SSID", '\\"ssid-ci\\"')])
-    print(" - [INFO] X55B_WIFI_SSID has been set for CI environment.")
-
-    projenv.ProcessUnFlags("-DX55B_WIFI_PASS=")
-    projenv.Append(CPPDEFINES=[("X55B_WIFI_PASS", '\\"pass-ci\\"')])
-    print(" - [INFO] X55B_WIFI_PASS has been set for CI environment.")
-
-if not ci_environment:
-    print("Checking required macros are set:")
-
-    for define in ["X55B_WIFI_SSID", "X55B_WIFI_PASS"]:
-        if define not in defines:
-            error_messages.append("{} is not defined.".format(define))
-        elif len(defines[define].strip('\\"')) == 0:
-            error_messages.append("{} has a zero-length value.".format(define))
+for define in ["X55B_WIFI_PASS", "X55B_WIFI_SSID"]:
+    if define not in defines:
+        error_messages.append("{} is not defined.".format(define))
+    elif len(defines[define].strip('\\"')) == 0:
+        error_messages.append("{} has a zero-length value.".format(define))
 
 
 if len(error_messages) > 0:
