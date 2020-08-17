433039fbe3f5a73b00e90e708d735fd2d70cea3e
Seth Fischer <seth@fischer.nz>
2020-08-17 19:11:17 +1200

    Trace messages via BluetoothSerial

--- include/debug/trace.h
+++ include/debug/trace.h
@@ -2,15 +2,17 @@
  * Debug via printing to the serial port.
  *
  * The Freematics ONE+ A and B devices do not make GPIO pins 12 to 15 available
- * for JTAG debugging. Therefore we must use Serial.println() to implement basic
- * tracing.
+ * for JTAG debugging. Therefore we use Serial.println() and SerialBT.println()
+ * to implement basic tracing.
  */
 
 #ifndef DEBUG_TRACE_H_
 #define DEBUG_TRACE_H_
 
 #ifdef DEBUG
-#define DEBUG_TRACE(message) Serial.println(message);
+#define DEBUG_TRACE(message) \
+    Serial.println(message); \
+    SerialBT.println(message);
 #else
 #define DEBUG_TRACE(x)
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #ifndef UNIT_TEST
 
 #include <Arduino.h>
+#include <BluetoothSerial.h>
 #include "debug/trace.h"
 #include "freematics_one_plus/pin_assignments.h"
+
+#ifdef DEBUG
+BluetoothSerial SerialBT;
+#endif
 
 void setup() {
 #ifdef DEBUG
     Serial.begin(115200);
+    SerialBT.begin("55b ONE+ debug");
     // Wait for serial port UART to connect.
     // Without this delay initial writes to the serial port are not captured.
     delay(2000);
