0a2755ab1cc2c40e758ada5b4b76f66e3efe5901
Seth Fischer <seth@fischer.nz>
2020-08-17 20:52:18 +1200

    Activate LED while initialising Serial and BluetootlSerial

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,22 @@ BluetoothSerial SerialBT;
 #endif
 
 void setup() {
+    pinMode(PIN_LED_BUILTIN, OUTPUT);
+    digitalWrite(PIN_LED_BUILTIN, LOW);
+
 #ifdef DEBUG
+    digitalWrite(PIN_LED_BUILTIN, HIGH);
+
     Serial.begin(115200);
     SerialBT.begin("55b ONE+ debug");
     // Wait for serial port UART to connect.
     // Without this delay initial writes to the serial port are not captured.
     delay(2000);
+
+    digitalWrite(PIN_LED_BUILTIN, LOW);
 #endif
 
     DEBUG_TRACE("setup() start")
-
-    pinMode(PIN_LED_BUILTIN, OUTPUT);
-    digitalWrite(PIN_LED_BUILTIN, LOW);
 
     DEBUG_TRACE("setup() end")
 }
