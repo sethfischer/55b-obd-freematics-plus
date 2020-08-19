8d21cc12acf4b3b95753238864054c1c59711d17
Seth Fischer <seth@fischer.nz>
2020-08-19 19:27:03 +1200

    Use PIN_LED from FreematicsPlus library

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,19 @@
 
 #include <Arduino.h>
 #include <BluetoothSerial.h>
+#include <FreematicsPlus.h>
 #include "Debug/Trace.h"
-#include "FreematicsOnePlus/PinAssignments.h"
 
 #ifdef DEBUG
 BluetoothSerial SerialBT;
 #endif
 
 void setup() {
-  pinMode(PIN_LED_BUILTIN, OUTPUT);
-  digitalWrite(PIN_LED_BUILTIN, LOW);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
 
 #ifdef DEBUG
-  digitalWrite(PIN_LED_BUILTIN, HIGH);
+  digitalWrite(PIN_LED, HIGH);
 
   Serial.begin(115200);
   SerialBT.begin("55b ONE+ debug");
@@ -22,7 +22,7 @@ void setup() {
   // Without this delay initial writes to the serial port are not captured.
   delay(2000);
 
-  digitalWrite(PIN_LED_BUILTIN, LOW);
+  digitalWrite(PIN_LED, LOW);
 #endif
 
   DEBUG_TRACE("[DEBUG] setup() start")
